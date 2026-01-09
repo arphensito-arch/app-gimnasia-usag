8118426ec758cb3b11d15fc6252de2f877870339
arphensito-arch <arphensito@gmail.com>
2026-01-09 04:47:26 -0300

    Update app_gimnasia.py

--- app_gimnasia.py
+++ app_gimnasia.py
@@ -310,9 +310,9 @@ def mostrar_dashboard():
             n = c2.text_input("Nombre")
             r = c3.selectbox("Rol", ["Gimnasta", "Entrenador"])
             if "Rol" : "Gimnasta"
-                p = st.selectbox("2. Nivel", ["Desarrollo (Nivel 3-5)", "Opcional/Elite (Nivel 6-10)"])
-                else 
-                    st.text_input("Password")
+            p = st.selectbox("2. Nivel", ["Desarrollo (Nivel 3-5)", "Opcional/Elite (Nivel 6-10)"])
+            else 
+            st.text_input("Password")
             if st.form_submit_button("Agregar"):
                 new = pd.DataFrame([{"DNI":d, "Nombre":n, "Rol":r, "Nivel_o_Pass":p, "Activo":"SI"}])
                 if actualizar_usuarios_db(pd.concat([df_users, new], ignore_index=True)):
@@ -363,6 +363,7 @@ if not st.session_state['logueado']: login()
 else:
     if st.session_state['rol_actual'] == 'Entrenador': mostrar_dashboard()
     else: mostrar_app_gimnasta()
+
 
 
 
